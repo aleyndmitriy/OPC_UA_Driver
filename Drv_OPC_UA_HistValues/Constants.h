9d7d508ce7e93d1c9a61a346cfc7e051f1662c09
Dmitrij_Aleinikov <Dmitrij_Aleinikov@stylesoft.by>
2019-12-04 11:14:05 +0300

    Rename driver

--- Drv_OPC_UA_HistValues/Constants.h
+++ Drv_OPC_UA_HistValues/Constants.h
@@ -1,12 +1,12 @@
 #pragma once
 const int VERSION_MAJOR = 1;
 const int VERSION_MINOR = 0;
-static const TCHAR* OPC_UA_LIBRARY_NAME = TEXT("Drv_OPC_UA_HistValues.dll");
+static const TCHAR* OPC_UA_LIBRARY_NAME = TEXT("Drv_OPC_UA_HdaItems.dll");
 static const TCHAR* OPC_UA_HIST_VALUES = TEXT("OPC_UA_HISTORY_VALUES");
-static const TCHAR* OPC_VENDOR = TEXT("MYOPC");
-static const TCHAR* OPC_UA_HDA_ITEM_DRIVER = TEXT("OPC_UA History Database Driver");
+static const TCHAR* OPC_VENDOR = TEXT("Open communication protocols");
+static const TCHAR* OPC_UA_HDA_ITEM_DRIVER = TEXT("OPC");
 static const TCHAR* OPC_UA_ITEM_DESC = TEXT("Items from History Database");
-static const TCHAR* OPC_UA_CLIENT_NAME = TEXT("OPC_UA Client");
+static const TCHAR* OPC_UA_CLIENT_NAME = TEXT("OPC UA historical values");
 const size_t MAX_DRIVERNAME_LENGTH = 63;
 const size_t MAX_DATABASENAME_LENGTH = 127;
 const size_t STR_LENGTH = 4095;
