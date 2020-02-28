e5a36c76c5a53712b394b050af2a310dd188458c
aleyndmitriy <shklov-2005@yandex.ru>
2020-02-28 13:42:52 +0300

    UI was corrected

--- Drv_OPC_UA_HistValues/resource.h
+++ Drv_OPC_UA_HistValues/resource.h
@@ -1,6 +1,6 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Drv_OPC_UA_HistValues.rc
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Drv_OPC_UA_HistValues.rc
 //
 #define IDD_CLIENT_SETTINGS_DLG         101
 #define IDD_DIALOG_SELECT_NODE          102
@@ -25,14 +25,15 @@
 #define IDC_COMBO_POLICY_ID             1018
 #define IDC_EDIT_USER_PASSWORD          1019
 #define IDC_NETWORK_TREE                1020
+#define IDC_DATA_QUALITY                1021
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_CONTROL_VALUE         1022
 #define _APS_NEXT_SYMED_VALUE           1008
 #endif
 #endif
